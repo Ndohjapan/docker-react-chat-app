98c2f4a90a5e6905cfa06514cfba6edff734f8cf
Joel <ndohjoelmbj16@gmail.com>
2023-04-18 23:32:48 +0100

    made some important update to the server and react container

---
+++ simpleServer/config/production.js
@@ -0,0 +1,10 @@
+module.exports = {
+    database: {
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: process.env.DB_PORT,
+      url: `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/devDatabase?authSource=admin`
+    },
+  };
+  
